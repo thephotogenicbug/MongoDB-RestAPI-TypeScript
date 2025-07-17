d9ea3bd72b9c63696d98a68d7cf605df84da8063
thephotogenicbug <navinkumarachari@gmail.com>
2025-07-17 16:06:46 +0530

    setup index ts file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello typescript");
+import express from "express";
+import "dotenv/config";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
